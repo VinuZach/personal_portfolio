8e4045d557251adbc638b87e2e6e855c2bf1ff1c
vinu <mathewzchrh603@gmail.com>
2024-09-17 19:33:28 +0530

    server settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5tn-jx96mqfky39biv7hupoaktzk_yxj#^ep48j5#52g+fnioc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
